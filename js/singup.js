2719ae840b28feb8800198ba847031639d551eb8
NatalyCortez <flyingchair_pe12@hotmail.com>
2018-01-08 12:57:17 -0500

    modificando signup.js e index signup.html

---
+++ js/singup.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+  $('select').material_select();
+//   var $telephone = $('#telephone');
+//   var $nextbtn = $('#next-btn');
+//   var $country = $('#country');
+//   $telephone.find("option[value='+51']").remove();
+//   $country.on('change',function(){
+//     if($country.val() === 'per'){
+//       $telephone.val('+51');
+//     }
+//     else if($country,val() === 'col'){
+//       $telephone.prop("value",'+57');
+// //   }
+// //   else {
+// //     $telephone.text('+52');
+// //  }
+//   });
+// $telephone.on('input',function(){
+//   if($telephone.val().length == 10) {  
+//     $nextbtn.prop('disabled',false);
+//     console.log($telephone.val());
+//   }
+//   else {
+//     $nextbtn.prop(disabled,true);    
+//   }
+// });
+});
